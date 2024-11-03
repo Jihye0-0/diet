320cbdaccd35ff9554a76a8a5bf637fac561d945
test <jihyewon1113@naver.com>
2024-11-03 22:43:47 +0900

    initial

--- android/diet/app/src/main/java/com/example/diet/MainActivity.kt
+++ android/diet/app/src/main/java/com/example/diet/MainActivity.kt
@@ -5,59 +5,43 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.diet.view.DietCalender
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.diet.ui.theme.DietTheme
 
-
-
-
-
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DietTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-//                    topBar = {
-//                        TopAppBar(
-//                            title = { Text("홈화면")},
-//                            colors = topAppBarColors(
-//                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-//                                titleContentColor = MaterialTheme.colorScheme.primary,
-//                            )
-//                        )
-//                    }
-                ) { innerPadding ->
-
-                    val none = innerPadding
-
-                    DietCalender()
-//                    TodayFood()
-
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
 }
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-
-//@Preview( showBackground = true, showSystemUi = true )
-//
-//@Composable
-//fun GreetingPreview() {
-//    DietTheme {
-
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DietTheme {
+        Greeting("Android")
+    }
+}
--- android/diet/app/src/main/java/com/example/diet/view/TodayFood.kt
+++
@@ -1,5 +0,0 @@
-package com.example.diet.view
-
-fun TodayFood() {
-
-}
